918cfc1662e6d6b2a4b2cdaeabc8506e5c7a796a
denis-torre <denis.torre@mssm.edu>
2019-02-28 15:35:13 -0500

    Baron updates

--- baron/pipeline/pipeline-baron.py
+++ baron/pipeline/pipeline-baron.py
@@ -12,8 +12,9 @@
 #############################################
 ##### 1. Python modules #####
 from ruffus import *
-import sys, os
+import sys, os, json
 import pandas as pd
+import numpy as np
 from rpy2.robjects import r, pandas2ri
 pandas2ri.activate()
 
@@ -25,6 +26,11 @@ import Baron as P
 # Alignment
 sys.path.append('../alignment/pipeline')
 import align
+sys.path.append('../../jupyter-notebook/biojupies-plugins/library/analysis_tools/enrichr/')
+import enrichr
+sys.path.append('../../jupyter-notebook/biojupies-plugins/library/core_scripts/shared/')
+import shared
+
 
 #############################################
 ########## 2. General Setup
@@ -133,17 +139,120 @@ def mergeKallisto(infiles, outfile):
 	# Write
 	count_dataframe.to_csv(outfile, sep='\t')
 
-
 #######################################################
 #######################################################
-########## S. 
+########## S3. Differential Expression
 #######################################################
 #######################################################
 
 #############################################
-########## . 
+########## 1. limma
 #############################################
 
+@follows(mkdir('s3-differential_expression.dir'))
+
+@subdivide(mergeStar,
+		   formatter(),
+		   's3-differential_expression.dir/baron-*',
+		   's3-differential_expression.dir/baron-')
+
+def runLimma(infile, outfiles, outfileRoot):
+
+	# Loop through comparisons
+	for comparison in [('P1', 'P3')]:
+
+		# Get outfile
+		outfile = '{outfileRoot}{comparison[0]}_vs_{comparison[1]}-limma.txt'.format(**locals())
+
+		# Fix comparison
+		comparison = np.array(['group'+x for x in comparison])
+
+		# Run limma
+		r.run_limma(infile, outfile, np.array(comparison))
+
+#######################################################
+#######################################################
+########## S4. Enrichr
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Enrichr
+#############################################
+
+@follows(mkdir('s4-enrichr.dir'))
+
+@transform(runLimma,
+           regex(r'.*/(.*)-limma.txt'),
+		   r's4-enrichr.dir/\1-listids.json')
+
+def runEnrichr(infile, outfile):
+
+	# Print
+	print('Doing {}...'.format(outfile))
+	control, perturbation = os.path.basename(infile).split('-')[1].split('_vs_')
+
+	# Read dataframe
+	limma_dataframe = pd.read_table(infile, index_col='gene_symbol').sort_values('t', ascending=False)
+
+	# Get N
+	n = 500
+
+	# Get genesets
+	listids = {
+		'up': enrichr.submit_enrichr_geneset(limma_dataframe.index[:n].tolist(), 'Genes upregulated in {perturbation} condition (vs {control})'.format(**locals())),
+		'down': enrichr.submit_enrichr_geneset(limma_dataframe.index[-n:].tolist(), 'Genes downregulated in {perturbation} condition (vs {control})'.format(**locals()))
+	}
+
+	# Write
+	with open(outfile, 'w') as openfile:
+		json.dump(listids, openfile, indent=4)
+
+#######################################################
+#######################################################
+########## S5. Enrichment Results
+#######################################################
+#######################################################
+
+#############################################
+########## 1. Get results
+#############################################
+
+@follows(mkdir('s5-enrichment_results.dir'))
+
+@transform(runEnrichr,
+           regex(r'.*/(.*)-listids.json'),
+           r's5-enrichment_results.dir/\1-enrichment.txt')
+
+def getEnrichmentResults(infile, outfile):
+
+	# Read IDs
+	with open(infile) as openfile:
+		enrichr_ids = json.load(openfile)
+
+	# Libraries
+	libraries = ['GO_Biological_Process_2018']
+
+	# Results
+	results = []
+
+	# Loop
+	for direction in ['up', 'down']:
+
+		# Get results
+		enrichment_results = shared.get_enrichr_results(enrichr_ids[direction]['userListId'], gene_set_libraries={x: x for x in libraries})
+		
+		# Add direction
+		enrichment_results['direction'] = direction
+
+		# Append
+		results.append(enrichment_results)
+		
+	# Concatenate
+	result_dataframe = pd.concat(results)
+
+	# Write
+	result_dataframe.to_csv(outfile, sep='\t', index=False)
 
 ##################################################
 ##################################################
--- baron/pipeline/scripts/baron.R
+++ baron/pipeline/scripts/baron.R
@@ -8,7 +8,6 @@
 ########## 1. Load libraries
 #############################################
 ##### 1. General support #####
-source('/Users/denis/Documents/Projects/scripts/Support.R')
 
 ##### 2. Other libraries #####
 
@@ -22,6 +21,47 @@ source('/Users/denis/Documents/Projects/scripts/Support.R')
 ########## 1. 
 #############################################
 
+run_limma <- function(infile, outfile, comparison) {
+    # Load
+    require(edgeR)
+
+    # Read counts
+    count_dataframe <- read.table(infile, header=TRUE, row.names='gene_symbol')
+
+    # Get design
+    group <- sapply(colnames(count_dataframe), function(x) strsplit(x, '.', fixed=TRUE)[[1]][1])
+    design <- model.matrix(~group+0)
+
+    # Create DGEList object
+    dge <- DGEList(counts=count_dataframe)
+    keep <- filterByExpr(dge, design)
+
+    # Calculate normalization factors
+    dge <- calcNormFactors(dge[keep,])
+
+    # Run VOOM
+    v <- voom(dge, plot=FALSE)
+
+    # Fit linear model
+    fit <- lmFit(v, design)
+
+    # Make contrast matrix
+    cmd <- paste("cont.matrix <- makeContrasts(", paste(comparison, collapse='-'), ", levels = design)", sep = '"')
+    eval(parse(text = cmd))
+
+    # Fit
+    fit2 <- contrasts.fit(fit, cont.matrix)
+
+    # Run DE
+    fit2 <- eBayes(fit2)
+
+    # Get results
+    limma_dataframe <- topTable(fit2, adjust='BH', number=nrow(count_dataframe))
+    limma_dataframe <- cbind(gene_symbol=rownames(limma_dataframe), limma_dataframe)
+
+    # Show results
+    write.table(limma_dataframe, outfile, quote=FALSE, sep='\t', row.names=FALSE)
+}
 
 #######################################################
 #######################################################
