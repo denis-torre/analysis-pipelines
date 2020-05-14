de430f3b6695a7b88b9ba5ee4b66f0ae6981e282
denis-torre <denis.torre@icahn.mssm.edu>
2020-05-14 11:14:23 -0400

    Dubois interface update

--- dubois/pipeline/website/app/app.py
+++ dubois/pipeline/website/app/app.py
@@ -17,10 +17,15 @@
 ########## 1. Load libraries
 #############################################
 ##### 1. Flask modules #####
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 from flask_basicauth import BasicAuth
 import os
+import json
+import plotly
+import plotly.graph_objects as go
 import pandas as pd
+import numpy as np
+# import numpy as np
 entry_point = '/dubois'
 app = Flask(__name__, static_url_path=os.path.join('/app/static'))
 
@@ -48,12 +53,31 @@ class PrefixMiddleware(object):
 			return ["This url does not belong to the app.".encode()]
 app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=entry_point)
 
+#############################################
+########## 2. Data
+#############################################ow toow
+##### 1. Files #####
+expression_file = 'app/static/data/kallisto-logcpm.txt'
+metadata_file = 'app/static/data/dubois-metadata.txt'
+groups_file = 'app/static/data/dubois-groups.json'
+
+##### 2. Data #####
+# Expression
+expression_dataframe = pd.read_csv(expression_file, index_col='gene_symbol', sep='\t')
+
+# Metadata
+metadata_dataframe = pd.read_csv(metadata_file, sep='\t')
+metadata_dataframe['sample_name'] = ['sample_'+str(x).replace('.', '_') for x in metadata_dataframe['Sample']]
+
+# Groups
+with open(groups_file) as openfile:
+	group_dict = json.load(openfile)
+
 #######################################################
 #######################################################
 ########## 2. Routes
 #######################################################
 #######################################################
-##### Handles routes used to generate notebooks.
 
 ##################################################
 ########## 2.1 Webpages
@@ -62,22 +86,77 @@ app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=entry_point)
 #############################################
 ########## 1. Home
 #############################################
-### Landing page for the website. Links to analyze() endpoint.
-### Links to: analyze().
+### Landing page for the website.
 
 @app.route('/')
 def index():
-	# Read samples
-	print(os.getcwd())
-	sample_dataframe = pd.read_table('app/static/dubois-metadata.txt')
-	conditions = sample_dataframe['Condition'].unique()
-	sample_conditions = ["Control", " + delta", " + delta, 1d", " + delta, 2d", " + delta, 3d", " + delta, 7d"]
-	conditions_dict = {
-		'show': sample_conditions,
-		'hide': list(set(conditions)-set(sample_conditions))
-	}
-	print(sample_dataframe)
-	return render_template('index.html', sample_dataframe=sample_dataframe, conditions_dict=conditions_dict)
 
+	# Return
+	return render_template('index.html', group_dict=group_dict)#, sample_dataframe=sample_dataframe, conditions_dict=conditions_dict)
+
+##################################################
+########## 2.2 APIs
+##################################################
+
+#############################################
+########## 1. Genes
+#############################################
+
+@app.route('/api/genes')
+
+def genes_api():
+
+	# Get json
+	genes_json = json.dumps([{'gene_symbol': x} for x in expression_dataframe.index])
+
+	# Return
+	return genes_json
+
+#############################################
+########## 2. Plot
+#############################################
+
+@app.route('/api/plot', methods=['GET', 'POST'])
+
+def plot_api():
+
+	# Get data
+	data = request.json
+	gene_symbol = data['gene_symbol']
+	conditions = data['conditions']
+	
+	# Melt data
+	melted_dataframe = expression_dataframe.loc[gene_symbol].rename('logcpm').rename_axis('sample_name').reset_index().merge(metadata_dataframe, on='sample_name')
+
+	# Get plot dataframe
+	plot_dataframe = melted_dataframe.groupby('Condition')['logcpm'].agg([np.mean, np.std, lambda x: list(x)])#.rename(columns={'<lambda>': 'points'})#.reindex(conditions)
+	plot_dataframe = plot_dataframe.rename(columns={plot_dataframe.columns[-1]: 'points'})
+	
+	# Rename groups
+	rename_dict = pd.DataFrame([x for key, value in group_dict.items() for x in value]).set_index('group_string').to_dict()['group_label']
+
+	# Initialize figure
+	fig = go.Figure()
+
+	# Loop
+	for condition in conditions:
+		fig.add_trace(go.Box(name=rename_dict[condition], y=plot_dataframe.loc[condition, 'points'], boxpoints='all', pointpos=0))
+	
+	# Layout
+	fig.update_layout(
+		title = {'text': gene_symbol+' gene expression', 'x': 0.5, 'y': 0.85, 'xanchor': 'center', 'yanchor': 'top'},
+		xaxis_title = 'Condition',
+		yaxis_title = 'Expression<br>(log10 counts per million)',
+		showlegend = False
+	)
+
+	# Return
+	return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+#######################################################
+#######################################################
+########## 3. Run App
+#######################################################
+#######################################################
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
